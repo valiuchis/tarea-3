9602d2f5b8748a818b7148d5c8f333cb7f118224
valiuchis <valeriacrc1301@outlook.com>
2022-11-12 18:01:29 -0300

    Archivos base de Máquina Expendedora (Tarea 2)

--- progra3/src/progra3/Cliente.java
+++ progra3/src/progra3/Cliente.java
@@ -10,4 +10,36 @@ package progra3;
  */
 public class Cliente {
     
+    private Moneda pago;
+    private int id;
+    private Expendedora vendingMachine;
+    private Bebida bebida;
+    
+    public Cliente(Moneda moneda, int codigoID, Expendedora exp) {
+        
+        pago = moneda;
+        id = codigoID;
+        vendingMachine = exp;
+    }
+    
+    public Bebida compraBebida () {
+        
+        try { 
+            bebida = vendingMachine.ventaBebida(pago, id);
+        } catch (NoHayBebidaException exception) {
+            System.out.println(exception.getMessage());
+            
+        } catch (PagoIncorrectoException exception) {
+            System.out.println(exception.getMessage());
+            
+        } catch (PagoInsuficienteException exception) {
+            System.out.println(exception.getMessage());
+        }
+        return bebida;    
+    }
+    
+    public String saborBebida () {
+        return bebida.beber();
+    }
+    
 }
